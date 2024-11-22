779491761f52c24b0b46ae9446f17b88aed60d20
matheus.mmarcondes <matheus.mmarcondes@SJC0562930W10-1>
2024-11-22 10:17:50 -0300

    Adicionar arquivos de projeto.

---
+++ Ex.Randômico/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Ex.Randômico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lsbRand = new System.Windows.Forms.ListBox();
+            this.lsbSequ = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(133, 368);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(75, 54);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(602, 368);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 54);
+            this.btnSair.TabIndex = 1;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lsbRand
+            // 
+            this.lsbRand.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsbRand.FormattingEnabled = true;
+            this.lsbRand.Location = new System.Drawing.Point(189, 36);
+            this.lsbRand.Name = "lsbRand";
+            this.lsbRand.Size = new System.Drawing.Size(120, 290);
+            this.lsbRand.TabIndex = 2;
+            // 
+            // lsbSequ
+            // 
+            this.lsbSequ.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lsbSequ.FormattingEnabled = true;
+            this.lsbSequ.Location = new System.Drawing.Point(509, 36);
+            this.lsbSequ.Name = "lsbSequ";
+            this.lsbSequ.Size = new System.Drawing.Size(120, 290);
+            this.lsbSequ.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lsbSequ);
+            this.Controls.Add(this.lsbRand);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnMostrar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.ListBox lsbRand;
+        private System.Windows.Forms.ListBox lsbSequ;
+    }
+}
+
---
+++ Ex.Randômico/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex.Randômico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            lsbRand.Items.Clear();
+            lsbSequ.Items.Clear();
+
+            int[] valores = new int[10];
+            Random random = new Random();
+
+            for (int i = 0; i < valores.Length; i++)
+            {
+                valores[i] = random.Next(1, 101);
+            }
+
+            foreach (int valor in valores)
+            {
+                lsbRand.Items.Add(valor);
+            }
+
+            Array.Sort(valores);
+
+            foreach (int valor in valores)
+            {
+                lsbSequ.Items.Add(valor);
+            }
+        }
+    }
+}
